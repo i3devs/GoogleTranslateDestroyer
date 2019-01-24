3f72b3491f19e9a929d8cf25436c4cf7cd9307cb
MatanDavidi <davidi.matan@gmail.com>
2019-01-24 22:35:34 +0100

    Defined functions "index" and "destroy": index redirects to the application's page without sending any input or output data, destroy redirects to the application's page sending the input back to the page and sending the destroyed input as output

--- destroyer/views.py
+++ destroyer/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .GTranslateDestroyer import GTranslateDestroyer
+from .models import Destroyer
+
+
+def index(request):
+    context = {'input': "", 'output': "", 'langs': Destroyer.LANGS, 'default': "en"}
+    return render(request, 'destroyer/index.html', context)
+
+
+def destroy(request):
+    if request.method == 'POST':
+        text = request.POST.get('input', None)
+        if len(text) > 0:
+            destroyer = GTranslateDestroyer(text)
+            output = destroyer.destroy()
+            context = {'input': text, 'output': output, 'langs': Destroyer.LANGS, 'default': "en"}
+            return render(request, 'destroyer/index.html', context)
+        else:
+            return render(request, 'destroyer/index.html')
+    else:
+        context = {'input': "", 'output': "", 'langs': Destroyer.LANGS, 'default': "en"}
+        return render(request, 'destroyer/index.html', context)
