c637800fc5a4e4033d760fb15eecd0f3f8c5d97d
Matan Davidi <davidi.matan@gmail.com>
2019-06-24 09:29:08 +0200

    Add virtual environment so the person that pulls this repository doesn't have to re-download all dependencies

---
+++ gtransdest_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Pattume\2019.06.18\GoogleTranslateDestroyer\gtransdest_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
